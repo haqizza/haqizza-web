52df2937d695443bf4675da30fc6c94747e7c88e
haqizza <haqizza@gmail.com>
2026-06-04 14:36:19 +0700

    feat: :sparkles: updated Projects page
    Added new local entries for Projects scope.
    Added ProjectCard component.

---
+++ app/components/ProjectCard.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { Icon, NuxtImg, NuxtLink } from "#components";
+import { VueElement } from "vue";
+
+
+const props = defineProps({
+  title: String,
+  description: String,
+  url: String,
+  displayImageUrl: String,
+  imageAlt: String
+})
+
+console.log(props.description == undefined)
+
+</script>
+
+<template>
+  <div class="flex flex-col gap-6 items-start p-4 border border-blue-500 rounded-2xl overflow-hidden">
+    <div class="text-3xl font-bold">
+      {{ props.title }}
+    </div>
+    <slot name="description"></slot>
+    <p v-if="props.description">{{ props.description }}</p>
+    <div v-if="props.url" class="flex items-center gap-2">
+      <Icon name="mdi:link-variant" size="24"/> Access here:
+      <NuxtLink :to="props.url" external class="underline">{{ props.url }}</NuxtLink>
+    </div>
+    <NuxtImg
+      v-if="props.displayImageUrl"
+      class="w-full h-auto justify-self-end"
+      :src="props.displayImageUrl"
+      :alt="props.imageAlt"
+    />
+  </div>
+</template>
--- app/pages/projects.vue
+++ app/pages/projects.vue
@@ -3,6 +3,22 @@
 
 <template>
   <div class="my-12 text-center text-2xl font-bold">
-    {{ $t('welcome') }}
+    {{ $t('projects.title') }}
+  </div>
+
+  <div class="grid grid-cols-2">
+    <ProjectCard
+      title="Collaborative Problem Solving Web Application (Joint Research of UPI and IPB)"
+      url="https://github.com/haqizza/cps-v2"
+      displayImageUrl="https://media.licdn.com/dms/image/v2/D5616AQFOxvzbQvIlBQ/profile-displaybackgroundimage-shrink_200_800/B56Zwys9m1IcAU-/0/1770377187242?e=1782345600&v=beta&t=aItIxFapp7__D8s79fa05RRxbYIPk_OqRKd7CxQnp0w"
+      imageAlt="CPS Web sample image"
+    >
+      <template #description>
+        <p>
+          The project is to create a web application to test some unique questions for Collaborative Problem-Solving type questions for middle-high school and high school, and it is automatically graded a student's cognitive and collaborative ability based on Natural Language Processing.
+        </p>
+        <p>I am currently working on it again</p>
+      </template>
+    </ProjectCard>
   </div>
 </template>
