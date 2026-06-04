22eed56ffccad8c6b31f70f0850581080b0767d5
haqizza <haqizza@gmail.com>
2026-06-04 14:31:53 +0700

    fix: :bug: Fixed footer margin overflow

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -9,7 +9,7 @@ import NavBar from '~/components/NavBar.vue';
     <div class="p-2 md:p-4  lg:p-12">
       <slot />
     </div>
-    <footer class="my-12 py-4 flex flex-row justify-center items-center text-white border-t border-dashed">
+    <footer class="py-10 flex flex-row justify-center items-center text-white">
       <hr class="grow border-1 border-gray-700 mx-4">
       <span>by Muhammad Izzatul Haq | Built with Nuxt.</span>
       <hr class="grow border-1 border-gray-700 mx-4">
