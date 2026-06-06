76c325f917292c1bd6e326f62f485146568f27ea
haqizza <haqizza@gmail.com>
2026-06-06 20:08:31 +0700

    refactor: :coffin: remove unused entry

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,15 +1,4 @@
 export default defineI18nConfig(() => ({
   legacy: false,
   locale: 'en',
-  messages: {
-    en: {
-      welcome: 'Welcome',
-    },
-    id: {
-      welcome: 'Selamat Datang',
-    },
-    ja: {
-      welcome: 'ようこそ',
-    },
-  },
 }))
