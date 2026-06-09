a9bbc2ff143c580672c036be80549d14669bc098
haqizza <haqizza@gmail.com>
2026-06-09 11:30:19 +0700

    feat: :sparkles: added previous-next post navigation function
    using Query Collection Item Surroundings to fetch prev&next post.
    Use post numbering.
    Added second post for testing.

---
+++ app/components/NavigationCard.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { NuxtImg, NuxtLink } from "#components";
+
+const props = defineProps({
+  title: String,
+  text: String,
+  path: String,
+})
+
+</script>
+
+<template>
+  <div>
+    <div class="mb-2 font-bold text-gray-400">{{ props.text }}</div>
+    <NuxtLink
+      class="
+        p-4 flex flex-row gap-4 items-center
+        border border-blue-500 rounded-xl
+      "
+      :to="props.path"
+      >
+      <NuxtImg
+        src="/logo.png"
+        alt="Default post picture"
+        class="h-12 w-auto self-center"
+      />
+      <h1 class="text-xl font-medium">{{ props.title }}</h1>
+    </NuxtLink>
+  </div>
+</template>
--- app/pages/blog/[slug].vue
+++ app/pages/blog/[slug].vue
@@ -6,27 +6,73 @@ const route = useRoute()
 const { locale } = useI18n()
 const slug = computed(() => withLeadingSlash(String(route.params.slug)))
 
-const { data: page } = await useAsyncData('page-' + route.path, async () => {
-  // Build collection name based on current locale
-  const collection = ('content_' + locale.value) as keyof Collections
-  const content = await queryCollection(collection).path(route.path).first()
+const { data: page } = await useAsyncData('page-' + route.path,
+  async () => {
+    // Build collection name based on current locale
+    const collection = ('content_' + locale.value) as keyof Collections
+    const content = await queryCollection(collection).path(route.path).first()
 
-  // Optional: fallback to default locale if content is missing
-  if (!content && locale.value !== 'en') {
-    return await queryCollection('content_en').path(route.path).first()
+    // Optional: fallback to default locale if content is missing
+    if (!content && locale.value !== 'en') {
+      return await queryCollection('content_en').path(route.path).first()
+    }
+
+    return content
+  }, {
+    watch: [locale], // Refetch when locale changes
   }
+)
 
-  return content
-}, {
-  watch: [locale], // Refetch when locale changes
-})
+const { data: surround } = await useAsyncData('surrounding-' + route.path,
+  async () => {
+    const collection = ('content_' + locale.value) as keyof Collections
+    const content = await queryCollectionItemSurroundings(collection, route.path)
+
+    console.log(content)
+
+    return content
+  }, {
+    watch: [locale], // Refetch when locale changes
+  }
+)
 </script>
 
 <template>
-  <div class="max-w-[50%] mx-auto mb-4 text-center text-lg">{{ $t('blog.title') }}</div>
-  <article v-if="page" class="max-w-[50%] m-auto prose dark:prose-invert prose-h1:text-center">
-    <ContentRenderer :value="page" :prose="true" :data="{ tags: page.tags }"/>
-  </article>
+  <div class="max-w-[50%] mx-auto mb-4 text-center text-lg">
+    {{ $t('blog.title') }}
+  </div>
+  <main v-if="page" class="max-w-[50%] m-auto">
+    <article
+      class="
+        prose-lg dark:prose-invert
+        prose-h1:text-center prose-a:text-blue-400 hover:prose-a:text-blue-600
+      "
+    >
+      <ContentRenderer
+        :value="page"
+        :prose="true"
+        :data="{ tags: page.tags }"
+      />
+    </article>
+    <div class="w-full flex gap-2 items-center">
+      <hr class="w-full border-0.5 border-white">
+      ⚡
+    </div>
+    <div class="w-full flex justify-between gap-12">
+      <NavigationCard
+        v-if="surround?.[0]"
+        text="Previous Post"
+        :title="surround[0].title"
+        :path="surround[0].path"
+      />
+      <NavigationCard
+        v-if="surround?.[1]"
+        text="Next Post"
+        :title="surround[1].title"
+        :path="surround[1].path"
+      />
+    </div>
+  </main>
   <div v-else class="text-center">
     <h1 class="text-2xl font-bold">Page not found</h1>
     <p>This page doesn't exist in {{ $t('language.name') }} language.</p>
