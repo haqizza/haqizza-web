6c028b9f83ac5095f724a5f48e7b5771d38e19a7
haqizza <haqizza@gmail.com>
2026-07-08 15:15:41 +0700

    feat: :sparkles: Separate TagsDisplay to TagsDisplay and Tag component

---
+++ app/components/Tag.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+defineProps<{
+  text: String
+}>()
+</script>
+
+<template>
+  <span class="px-1 py-0.25 mx-1 border border-blue-500 bg-blue-800/50 rounded-md">
+    {{ text }}
+  </span>
+</template>
--- app/components/TagsDisplay.vue
+++ app/components/TagsDisplay.vue
@@ -7,8 +7,6 @@ defineProps<{
 
 <template>
   <div>
-    <span v-for="tag in tags" class="px-1 py-0.25 mx-1 border border-blue-500 bg-blue-800/50 rounded-md">
-      {{ tag }}
-    </span>
+    <Tag v-for="tag in tags" text="tag" />
   </div>
 </template>
