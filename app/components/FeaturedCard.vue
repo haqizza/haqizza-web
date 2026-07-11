5992801ffea915440620db9c04a4dd15a182ab59
haqizza <haqizza@gmail.com>
2026-07-11 21:46:53 +0700

    fix: :bug: Remove props that still using props. prefix

--- app/components/FeaturedCard.vue
+++ app/components/FeaturedCard.vue
@@ -5,14 +5,14 @@ defineProps<{
   title: string,
   description: string,
   path: string,
-  displayImageUrl: string
+  displayImageUrl?: string
 }>()
 
 </script>
 
 <template>
-  <NuxtLink class="p-4 flex flex-row gap-8 items-start border border-blue-500 rounded-xl overflow-hidden" :to="props.path">
-    <NuxtImg :src="displayImageUrl || '/logo.png'" alt="Default post picture" class="h-44 w-auto self-center" />
+  <NuxtLink class="p-4 flex flex-row gap-8 items-start border border-blue-500 rounded-xl overflow-hidden" :to="path">
+    <NuxtImg :src="displayImageUrl ?? '/logo.png'" alt="Default post picture" class="h-44 w-auto self-center" />
     <div>
       <h1 class="mb-3 text-xl font-medium">{{ title }}</h1>
       <p>{{ description }}</p>
